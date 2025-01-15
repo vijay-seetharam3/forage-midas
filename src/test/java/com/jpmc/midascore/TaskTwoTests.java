767bad596cd51d9ea69e229ba12c863771d903b2
vijay-seetharam3 <seetharamvijay3@gmail.com>
2025-01-15 16:35:24 +0530

    Task One Completed

--- src/test/java/com/jpmc/midascore/TaskTwoTests.java
+++ src/test/java/com/jpmc/midascore/TaskTwoTests.java
@@ -8,6 +8,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.annotation.DirtiesContext;
 
+
+
+
 @SpringBootTest
 @DirtiesContext
 @EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"})
